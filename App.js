e224b0e2a8d7a1acc37396a4a5ea6d63cd54e591
VioletaTaneva <violeta.akaunt11@gmail.com>
2024-08-28 04:39:54 +0200

    qr continent game added

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from "react";
-import { View } from "react-native";
-import PickYourImage from "./components/ImagePicker";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import PickYourImage from './components/PickYourImage';
+import LocationFinder from './components/LocationFinder';
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <PickYourImage />
+      <LocationFinder />
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
